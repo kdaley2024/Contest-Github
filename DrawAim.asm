64e1bc14ef5f331d5a9a8ed88f62f840e4a218ba
kdaley2024 <kdaley2024@my.fit.edu>
2025-10-19 15:05:06 -0400

    Update DrawAim.asm
    Added drawing logic for aim based on angle degrees.

--- DrawAim.asm
+++ DrawAim.asm
@@ -1,3 +1,7 @@
+INCLUDE rocketGame.inc
+
+.code
+
 DrawAim PROC USES eax ebx ecx edx esi edi,
     x0:DWORD, y0:DWORD, deg:SDWORD
     ; 0..180, then index 15-degree steps
@@ -40,5 +44,58 @@ cmp eax, 5
     mov esi, -1
     mov edi, 0
     jmp DrawAim_Lset
+DrawAim_L1:  mov esi, 1   ; 15
+             mov edi, 0
+             jmp DrawAim_Lset
+DrawAim_L2:  mov esi, 1   ; 30
+             mov edi, -1
+             jmp DrawAim_Lset
+DrawAim_L3:  mov esi, 1   ; 45
+             mov edi, -1
+             jmp DrawAim_Lset
+DrawAim_L4:  mov esi, 1   ; 60
+             mov edi, -1
+             jmp DrawAim_Lset
+DrawAim_L5:  mov esi, 0   ; 75
+             mov edi, -1
+             jmp DrawAim_Lset
+DrawAim_L6:  mov esi, 0   ; 90
+             mov edi, -1
+             jmp DrawAim_Lset
+DrawAim_L7:  mov esi, 0   ; 105
+             mov edi, -1
+             jmp DrawAim_Lset
+DrawAim_L8:  mov esi, -1  ; 120
+             mov edi, -1
+             jmp DrawAim_Lset
+DrawAim_L9:  mov esi, -1  ; 135
+             mov edi, -1
+             jmp DrawAim_Lset
+DrawAim_L10: mov esi, -1  ; 150
+             mov edi, 0
+             jmp DrawAim_Lset
+DrawAim_L11: mov esi, -1  ; 165
+             mov edi, 0
+DrawAim_Lset:
 
-
+    mov ecx, 10
+    mov eax, x0
+    mov ebx, y0
+@dots:
+    ; bounds check
+    cmp eax, 0    
+    jl  @next
+    cmp eax, scrW  
+    jge @next
+    cmp ebx, 0    
+    jl  @next
+    cmp ebx, scrH  
+    jge @next
+    ; draw dot
+    INVOKE PutChAt, '.', eax, ebx
+@next:
+    add eax, esi
+    add ebx, edi
+    loop @dots
+    ret
+DrawAim ENDP
