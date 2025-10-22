9e43a0fb9d90a119a3e57f33f24563f219baeaac
kdaley2024 <kdaley2024@my.fit.edu>
2025-10-22 11:34:16 -0400

    Update
    Implemented a procedure to increase game difficulty every 10 seconds based on the system tick count.

--- UpdateDifficulty.asm
+++ UpdateDifficulty.asm
@@ -1,2 +1,18 @@
 INCLUDE rocketGame.inc
 
+; 10s difficulty bump using GetTickCount
+UpdateDifficulty PROC USES eax
+    call GetTickCount@0
+    mov  edx, lastBumpMs
+    cmp  eax, edx
+    jb   @skip
+    mov  eax, bgSpeed
+    inc  eax
+    mov  bgSpeed, eax
+    call GetTickCount@0
+    add  eax, 10000
+    mov  lastBumpMs, eax
+@skip: ret
+UpdateDifficulty ENDP
+
+END
