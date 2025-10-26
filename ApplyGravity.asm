1ce53e06fae9e101f6c21d7d836b63826b725ecc
ChervelleP <cpierre2022@my.fit.edu>
2025-10-26 20:42:53 +0000

    Apply Gravity Module

--- ApplyGravity.asm
+++ ApplyGravity.asm
@@ -1,1 +1,13 @@
 INCLUDE rocketGame.inc
+
+
+.code
+ApplyGravity PROC USES eax
+    mov  eax, stickY
+    cmp  eax, 18
+    jge  @done
+    inc  eax
+    mov  stickY, eax
+@done: ret
+ApplyGravity ENDP
+END
