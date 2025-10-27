0e44c4a857e0b059a63148b58a94b45c1bc0ead3
kdaley2024 <kdaley2024@my.fit.edu>
2025-10-26 21:07:13 -0400

    finished

--- MoveShipDown.asm
+++ MoveShipDown.asm
@@ -1,1 +1,15 @@
 INCLUDE rocketGame.inc
+
+MoveShipDown PROC USES eax ebx
+    mov  eax, shipY
+    add  eax, shipStep
+    mov  shipY, eax
+
+    cmp  eax, scrH
+    jl   @done
+
+    ; respawn at top with new random X and size/HP
+    call SpawnShip
+@done:
+    ret
+MoveShipDown ENDP
