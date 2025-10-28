e88105f4541303d6809e491de4e84c827ba13b6b
ChervelleP <cpierre2022@my.fit.edu>
2025-10-28 20:11:55 +0000

    Helper procedure to put characters on the screen

---
+++ PutChAt.asm
@@ -0,0 +1,19 @@
+INCLUDE LimbbyLimb.inc
+.code
+
+;declare PutChAt parameters
+PutChAt PROTO :BYTE, :DWORD, :DWORD
+
+; Helper procedure that prints a single character at a specific x, y
+; coordinate on the console using Irvine32 library.
+
+PutChAt PROC USES eax edx, character:BYTE, x:DWORD, y:DWORD
+    mov   dl, BYTE PTR x
+    mov   dh, BYTE PTR y
+    call  Gotoxy
+    mov   al, character
+    call  WriteChar
+    ret
+PutChAt ENDP
+
+END
