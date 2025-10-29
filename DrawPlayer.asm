b5c8cf1e8e35ced7124a0c2fb7bc7d0559ccd845
kdaley2024 <kdaley2024@my.fit.edu>
2025-10-29 11:22:04 -0400

    Implement head drawing in DrawPlayer procedure
    Added head drawing logic for the stickman.

--- DrawPlayer.asm
+++ DrawPlayer.asm
@@ -10,6 +10,14 @@ LIGHTCYAN EQU 11
 
 ;this procedure will draw the stickman in parts
 DrawPlayer PROC USES eax ebx edx
+    @draw:
+        ; Draw the head looking like O
+        mov  ebx, limbMask
+        test ebx, 1
+        jz   @noHead
+        INVOKE PutChAt, 'O', stickX, eax
+    @noHead:
+
     ret
 DrawPlayer ENDP
 END
