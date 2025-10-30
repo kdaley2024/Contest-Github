79b9cac0e8b5e585c4b6c59b0da00daa339c61f3
kdaley2024 <kdaley2024@my.fit.edu>
2025-10-30 16:28:53 -0400

    Add ESC key handling to CheckKeys procedure

--- CheckKeys.asm
+++ CheckKeys.asm
@@ -8,6 +8,16 @@ VK_ESCAPE EQU 1Bh
 .code
 
 CheckKeys PROC USES eax ebx
+
+    ; ESC - quit the game
+    push VK_ESCAPE
+    call GetAsyncKeyState@4
+    test ax, 8000h
+    jz   @noEsc
+    mov  bgSpeed2x, 0FFFFFFFFh
+    ret
+@noEsc:
+
     ret
 CheckKeys ENDP
 END
