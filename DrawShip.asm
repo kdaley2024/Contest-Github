79af28100ee0928152d5b0a423ada53b30315c6b
kdaley2024 <kdaley2024@my.fit.edu>
2025-10-30 00:26:13 -0400

    Create DrawShip.asm for spaceship drawing
    Added procedure for drawing spaceships.

---
+++ DrawShip.asm
@@ -0,0 +1,13 @@
+INCLUDE LimbbyLimb.inc
+
+BLACK    EQU 0
+WHITE    EQU 15
+LIGHTRED EQU 12
+LIGHTCYAN EQU 11
+
+.code
+;this is the procedure for drawing the spaceships in different sizes.
+DrawShip PROC USES eax ebx ecx edx edi
+    ret
+DrawShip ENDP
+END
