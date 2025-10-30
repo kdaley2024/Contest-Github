c27e0bc3bb422c9ad8cdffe175b9eb2241b485e1
kdaley2024 <kdaley2024@my.fit.edu>
2025-10-30 00:31:13 -0400

    Add data section with messages and variables

--- main.asm
+++ main.asm
@@ -1,5 +1,10 @@
 INCLUDE LimbbyLimb.inc
 .data
+
+titleMsg    BYTE "Limb by Limb",0
+lvlLbl      BYTE " Level: ",0
+level           DWORD 1
+
 stickX      DWORD 6
 
 limbMask        DWORD 0Fh
@@ -7,6 +12,8 @@ limbMask        DWORD 0Fh
 stickHead   BYTE "O",0
 stickBody   BYTE "|",0
 stickArms   BYTE "/-",0
+
+shipType    DWORD 0
 
 .code
 main PROC PUBLIC
