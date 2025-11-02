af15ce0fc667481458d1a0b93b8104bf67ce8208
kdaley2024 <kdaley2024@my.fit.edu>
2025-11-02 00:00:04 -0400

    Enhance game difficulty in UpdateDifficulty.asm
    Increased game difficulty by adjusting speed and level.

--- UpdateDifficulty.asm
+++ UpdateDifficulty.asm
@@ -3,11 +3,21 @@ INCLUDE LimbbyLimb.inc
 ;this procedure increases the difficulty of the game making the spaceships appear and fall on the screen faster
 .code
 UpdateDifficulty PROC USES eax
-
+    ;WIN API that was learnt from the textbook
     call GetTickCount@0
     mov  edx, lastBumpMs
     cmp  eax, edx
     jb   @skip
+
+    ; +0.5 speed step
+    mov  eax, bgSpeed2x
+    inc  eax
+    mov  bgSpeed2x, eax
+
+    ;the level increases which means that the number with the title level will increase in the HUD
+    mov  eax, level
+    inc  eax
+    mov  level, eax
 
 @skip:
     ret
