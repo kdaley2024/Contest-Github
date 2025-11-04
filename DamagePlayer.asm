787cb84413db9fe63a5ad2a7dd64a272bea68192
kdaley2024 <kdaley2024@my.fit.edu>
2025-11-04 11:57:15 -0500

    Modify DamagePlayer procedure to include damage logic

--- DamagePlayer.asm
+++ DamagePlayer.asm
@@ -6,6 +6,16 @@ DamagePlayer PROC USES eax ebx ecx edx
     mov  eax, nextDamageIdx
     cmp  eax, 4
     jge  @done
+
+    mov  ecx, eax
+    shl  ecx, 2
+    mov  ebx, damageOrder[ecx]
+    not  ebx
+    and  limbMask, ebx
+
+    inc  eax
+    mov  nextDamageIdx, eax
+
 @done:
     ret
 DamagePlayer ENDP
