7e5fc85f90d21b9827678eea2e47b9ebf5551e61
kdaley2024 <kdaley2024@my.fit.edu>
2025-11-04 11:51:20 -0500

    Create DamagePlayer.asm

---
+++ DamagePlayer.asm
@@ -0,0 +1,8 @@
+INCLUDE LimbbyLimb.inc
+
+.code
+;this procedure includes the logic for how the player will flash when hit by a spaceship, and lose a limb per hit
+DamagePlayer PROC USES eax ebx ecx edx
+    ret
+DamagePlayer ENDP
+END
