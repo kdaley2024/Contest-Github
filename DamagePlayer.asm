80901cf893403a4d725e3ca2d327f25b88a55bc5
kdaley2024 <kdaley2024@my.fit.edu>
2025-11-04 11:53:47 -0500

    Implement damage index check in DamagePlayer
    Add logic to check damage index before processing.

--- DamagePlayer.asm
+++ DamagePlayer.asm
@@ -3,6 +3,10 @@ INCLUDE LimbbyLimb.inc
 .code
 ;this procedure includes the logic for how the player will flash when hit by a spaceship, and lose a limb per hit
 DamagePlayer PROC USES eax ebx ecx edx
+    mov  eax, nextDamageIdx
+    cmp  eax, 4
+    jge  @done
+@done:
     ret
 DamagePlayer ENDP
 END
