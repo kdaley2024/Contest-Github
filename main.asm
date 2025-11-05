1b5a56e3d74d51303e8bbc8101e1e91d2f5eb65b
kdaley2024 <kdaley2024@my.fit.edu>
2025-11-05 11:17:11 -0500

    Refactor game loop and timing logic in main.asm

--- main.asm
+++ main.asm
@@ -43,47 +43,68 @@ playerFlash     DWORD 0
 
 .code
 main PROC PUBLIC
+;wipes the screen clean
     call Clrscr
+;get random numbers ready
     call Randomize
-
+;get the time now in ms
     call GetTickCount@0
+;add 15 seconds
     add  eax, 15000
+;remember when to make it harder
     mov  lastBumpMs, eax
-
+;make the first ship
     call SpawnShip
 
 gameLoop:
+;if chosen to quit, leave the game
     cmp  bgSpeed2x, 0FFFFFFFFh
     je   exitGame
-
+;clear the screen for a new frame
     call Clrscr
+;read arrow keys/ESC
     call CheckKeys
+;turn speed into a step size
     call UpdateSteps
+;move the ship down
     call MoveShipDown
+;checks if the ship hits the player
     call CheckPlayerShipCollision
 
+;checks if the player still flashing red
     cmp  playerFlash, 0
     jle  @noflash
+;count down the flash
     dec  playerFlash
 @noflash:
+;checks if the player is invincible
     cmp  playerIFrames, 0
     jle  @noifr
+;counts down the invinvibility
     dec  playerIFrames
 @noifr:
 
+;draw the ship
     call DrawShip
+;draw the title and the level headers
     call DrawHUD
+;draw the stickman
     call DrawPlayer
+;make the game harder over time
     call UpdateDifficulty
     
-
+;how long to pause the frame
     push frameDelay
+;pause so it doesn't run too fast
     call Sleep@4
 
+;do the next frame
     jmp  gameLoop
 
 exitGame:
+;clear the screen
     call Clrscr
+;close the program
     INVOKE ExitProcess, 0
 main ENDP
 END main
